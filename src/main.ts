3a1688640eea650659b43d4b565ffadcc96a956c
홍태호 <taeho.h95@gmail.com>
2026-07-05 20:43:24 +0900

    fix: allow production web origin

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { setupServiceSwagger } from "./service/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: "https://opod-web.vercel.app" });
   setupServiceSwagger(app, [ServiceModule]);
   await app.listen(process.env.PORT ?? 3000);
 }
